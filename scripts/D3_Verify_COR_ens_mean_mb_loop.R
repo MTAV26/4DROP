f98b2c8de52b222a439adee9d226d229a7894c9f
Miguel Ángel Torres Vázquez <77393196+MTAV26@users.noreply.github.com>
2023-10-07 20:29:56 +0200

    D3_Verify_COR_ens_mean_mb_loop.R

--- scripts/D3_Verify_COR_ens_mean_mb_loop.R
+++ scripts/D3_Verify_COR_ens_mean_mb_loop.R
@@ -1,10 +1,9 @@
 
-
 rm(list = ls())
 graphics.off()
 gc()
 
-#library(ncdf)
+library(ncdf4)
 library(sp)
 library(maptools) # loads sp library too
 library(RColorBrewer) # creates nice color schemes
@@ -16,50 +15,36 @@ library(pracma)
 library(verification)
 library(psych)
 
+source("~/Dropbox/4DROP/script/Common/CorrMIO.R")
+source("~/Dropbox/4DROP/script/Common/ColorBarM.R")
+source("~/Dropbox/4DROP/script/Common/mioplot_global.R")
 
+dir_drop = '~/Dropbox/4DROP/DROP/'
+dir_4drop = '~/Dropbox/4DROP/Data/'
+dir_spi= '~/Dropbox/4DROP/Data/spi/'
+dir_out2= '~/Dropbox/4DROP/Correlaciones/231006_correlation/corre1/'
 
-# source("/home/miguel/4DROP/script/Common/CorrMIO.R")
-# source("/home/miguel/4DROP/script/Common/ColorBarM.R")
-# source("/home/miguel/4DROP/script/Common/mioplot_global.R")
-
-source("C:/Users/Usuario/Dropbox/4DROP/script/Common/CorrMIO.R")
-source("C:/Users/Usuario/Dropbox/4DROP/script/Common/ColorBarM.R")
-source("C:/Users/Usuario/Dropbox/4DROP/script/Common/mioplot_global.R")
-
-
-## fixed parameters
-#time_scale = c(1, 3, 6, 12)
-time_scale = c(6)
-data(wrld_simpl)
-
-# dir_drop = '/home/miguel/4DROP/DROP/'
-# dir_oss = '/home/miguel/4DROP/results/spi/'
-# dir_out= '/home/miguel/4DROP/results/cor'
-
-
-dir_drop = 'C:/Users/Usuario/Dropbox/4DROP/DROP/'
-dir_oss = 'C:/Users/Usuario/Dropbox/4DROP/spi/'
-dir_out= 'C:/Users/Usuario/Dropbox/4DROP/cor'
 
 anni = 1981:2020
 mesi = rep(1:12, length(anni))
 
+#SON 36 ANNI
+anniok = 36 # 33/37 ?? circa il 90%
 
 load(file.path(dir_drop, "lon_GPCP_1981_2017.RData"))
 load(file.path(dir_drop, "lat_GPCP_1981_2017.RData"))
 lonGPCP = lon
 latGPCP = lat
-lat2 = lat[which(lat > -60 & lat < 85)]
 
-forecast= c("ESP", "S5")
+#mesi= 1:40
+datasets = c(
+  'MSWEP', 'ERA5', 'CHIRPS', 'NCEP','MERRA2', 'CPC',
+  'PRECL', 'CAMS_OPI', 'GPCC',   'GPCP','JRA55',
+  'ENS' )
 
-datasets = c('MSWEP', 'ERA5',     'CHIRPS', 'NCEP','MERRA2', 'CPC',
-             'PRECL', 'CAMS_OPI', 'GPCC',   'GPCP','JRA55')
-
-# brk_cor <- seq(-1, 1, 0.2)
-# col_cor <- (colorRampPalette(brewer.pal(11, "PRGn"))(10))
-
-start_dates = c(5, 7, 8, 10, 2, 4, 1, 11)
+time_scale = c(6)
+start_dates = c(01, 02, 04, 05, 07, 08, 10, 11)
+pd= c("S5")
 
 for (isc in 1:length(time_scale)) {
   sc = time_scale[isc]
@@ -67,158 +52,122 @@ for (isc in 1:length(time_scale)) {
   for (idata in 1:length(datasets)) {
     dataset = datasets[idata]
     
-     for (forst in 1:length(forecast)) {
-       pd = forecast[forst]
-
+    for (istart_date in 1:length(start_dates)) {
+      start_date = start_dates[istart_date]
       
-      for (istart_date in 1:length(start_dates)) {
-        start_date = start_dates[istart_date]
-        
-
-        if (start_date == 7) {
-          target_season = 'JJA'
-        } else if (start_date == 5) {
-          target_season = 'JJA'
-        } else if (start_date == 8) {
-          target_season = 'SON'
-        } else if (start_date == 10) {
-          target_season = 'SON'
-        } else if (start_date == 2) {
-          target_season = 'MAM'
-        } else if (start_date == 4) {
-          target_season = 'MAM'
-        } else if (start_date == 1) {
-          target_season = 'DJF'
-        } else if (start_date == 11) {
-          target_season = 'DJF'
-        }
-        
-        
-        
-        if (target_season == 'MAM') {
-          mesi_8 = which(mesi== 05)
-        } else if (target_season == 'JJA') {
-          mesi_8 = which(mesi == 08)
-        } else if (target_season == 'SON') {
-          mesi_8 = which(mesi == 11)
-        } else if (target_season == 'DJF') {
-          mesi_8 = which(mesi == 02)
-        }
+      # dates = seq(start_date, start_date + 3)
+      if (start_date == 5) {
+        target_season = 'JJA'
+      } else if (start_date == 7) {
+        target_season = 'JJA'
+      } else if (start_date == 8) {
+        target_season = 'SON'
+      } else if (start_date == 10) {
+        target_season = 'SON'
+      } else if (start_date == 2) {
+        target_season = 'MAM'
+      } else if (start_date == 4) {
+        target_season = 'MAM'
+      } else if (start_date == 1) {
+        target_season = 'DJF'
+      } else if (start_date == 11) {
+        target_season = 'DJF'
+      }
       
       
-      brk <- seq(-1, 1, 0.2)
-      col <- (colorRampPalette(brewer.pal(11, "PRGn"))(10))
+      if (target_season == 'MAM') {
+        mesi_8 = which(mesi== 05)
+      } else if (target_season == 'JJA') {
+        mesi_8 = which(mesi == 08)
+      } else if (target_season == 'SON') {
+        mesi_8 = which(mesi == 11)
+      } else if (target_season == 'DJF') {
+        mesi_8 = which(mesi == 02)
+      }
       
-      nam <- paste('corre2')
+      
+      # Leemos  LOS ONCE MIEMBROS DE DROP
+      nam <- paste("spi", sep = "")
       print(nam)
-      obs_bs = array(data = NA, dim = c(length(lon), length(lat2), 11))
       
-      load(file.path(paste(dir_out,"/COR_S5_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_GPCP_original.RData", sep = "") ))
-      aux = get(nam)
-      obs_bs[, , 1] = aux[,]
+      if (dataset != "ENS") {
+        load(file.path(dir_drop,paste("SPI", sc, "_",dataset,"_1981_2020.RData", sep = "")))
+        data = spi6[,,mesi_8]
+      } else if (dataset == "ENS") {
+        load(file.path(dir_drop,paste("SPI", sc, "_",dataset,"_1981_2020.RData", sep = "")))
+        data = spi[,,mesi_8]
+      }
+      # Leemos las predicciones de 4Drop
+      nam <- paste("spi",sc,"pred", sep = "")
+      print(nam)
+      load(file.path(dir_spi, paste("S5_SPI", sc,"_",sprintf("%02d", start_date),  "_",  target_season, "_ENS.RData", sep = "" )))
+      ens = spi6pred
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_CAMS_OPI_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 2] = aux[ , ]
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_CHIRPS_original.RData",sep = "") ))
-      aux = get(nam)
-      obs_bs[, , 3] = aux[ , ]
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_CPC_original.RData",sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 4] = aux[ , ]
+      data[is.infinite(data)]=NA
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_GPCC_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 5] = aux[ , ]
+      ni = dim(data)[1]
+      nj = dim(data)[2]
+      nt = dim(data)[3]
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_JRA55_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 6] = aux[ , ]
+      corre <- matrix(data = NA,nrow = ni, ncol = nj)
+      pvalue <- matrix(data = NA, nrow = ni, ncol = nj)
       
-      load(file.path( paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                            "_",  target_season,"_PRECL_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 7] = aux[ , ]
+      corre_det <- matrix(data = NA,nrow = ni, ncol = nj)
+      pvalue_det <- matrix(data = NA, nrow = ni, ncol = nj)
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date), 
-                           "_",  target_season,"_ERA5_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 8] = aux[ , ]
       
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_NCEP_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 9] = aux[ , ]
-      
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season,"_MERRA2_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 10] = aux[ , ]
-      
-      load(file.path(paste(dir_out,"/COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                           "_",  target_season, "_MSWEP_original.RData", sep = "")  ))
-      aux = get(nam)
-      obs_bs[, , 11] = aux[ , ]
-      
-      bs_mean = (apply(obs_bs, c(1, 2), mean, na.rm = TRUE))
-      #bs_mean = bs_mean[, which(lat > -60 & lat < 85)]
-
-      
-      postscript(
-        file.path(dir_out,paste("COR_", pd, "_spi",sc,"_",sprintf("%02d", start_date),
-                                "_",  target_season,"_ens_mean_drop_original.eps",sep = "")),
-        paper = "special",
-        width = 11,
-        height = 7,
-        horizontal = T
-      )
+      for (i in 1:ni) {
+        for (j in 1:nj) {
+          
+          OK <- complete.cases(ens[i, j,], data[i, j,])
+          x <- ens[i, j, OK]
+          y <- data[i, j, OK]
+          n <- length(x)
+          #if (n >= anniok * 12) {
+          if (n >= nt*0.9) {
+            
+            dum = CorrMIO((x), (y), method = 'pearson', pval = TRUE)
+            corre[i, j] = as.numeric(dum)[1]
+            pvalue[i, j] <- as.numeric(dum)[4]
+            rm(dum)
+            
+          }
+          
+          
+          OK1 <- complete.cases(ens[i, j,], data[i, j,])
+          x1 <- ens[i, j, OK1]
+          y1 <- data[i, j, OK1]
+          n1 <- length(x1)
+          #if (n >= anniok * 12) {
+          if (n1 >= nt*0.9) {
+            
+            x1d = as.vector(detrend(x1, tt = 'linear', bp = c()))
+            y1d = as.vector(detrend(y1, tt = 'linear', bp = c()))
+            
+            dum = CorrMIO((x1d), (y1d), method = 'pearson', pval = TRUE)
+            corre_det[i, j] = as.numeric(dum)[1]
+            pvalue_det[i, j] <- as.numeric(dum)[4]
+            rm(dum)
+          }
+          
+          rm(OK, n, x, y, x1d, y1d, x1, y1, n1)
+        }
+      }
+      pvalue_adj = p.adjust(pvalue, method = "fdr", n = length(pvalue[!is.na(pvalue)]))
+      pvalue_adj = matrix(pvalue_adj, nrow = ni, ncol = nj)
+      # 
+      lat2 = lat[which(lat > -60 & lat < 85)]
+      corre2 = corre[, which(lat > -60 & lat < 85)]
+      pvalue2 = pvalue[, which(lat > -60 & lat < 85)]
+      pvalue_adj2 = pvalue_adj[, which(lat > -60 & lat < 85)]
       
     
-      layout(matrix(c(1, 2), 1, 2, byrow = T), widths = c(11, 1.5))
-      par(oma = c(1, 1, 4, 1))
-      tit <-
-        paste(
-           'Correlation for SPI6 in ',target_season,'; ', pd, ' against  DROP members'
-           #\n Start date: ',
-          # month.name[ start_date],
-          # ' - Period: 1981/2020\n'
-          , sep=""
-        )
-      
-      
-      mioplot(bs_mean, lon,lat2,
-              toptitle = '',
-              sizetit = 0.8,
-              brks = brk,  
-              cols = col,
-              axelab =F, 
-              filled.continents = FALSE,
-              drawleg = F)
-      title(tit, line = 0.5, outer = T)
-      ce = 1.4
-      ColorBar(
-        brks = brk,
-        cols = rev(col),
-        vert = T
-      )
-      
-      save(corre2, file = file.path(dir_out,
-                                    paste("COR_", pd, "_spi",sc,"_",
-                                          sprintf("%02d", start_date),  "_",
-                                          target_season,"_ENS_MEAN_original.RData",
-                                          sep = "") ))
-      dev.off()
-      
-   }
-   }
+      save(corre2, file = file.path(dir_out2, paste("COR_",pd,"_spi",sc,"_",sprintf("%02d", start_date),"_",target_season,"_",dataset,"_original.RData", sep = "") ))
+      save(pvalue_adj2, file = file.path(dir_out2, paste("pvalue_",pd,"_spi",sc,"_",sprintf("%02d", start_date),"_",target_season,"_",dataset,"_original.RData", sep = "") ))
+    }
+  }
 }
-}
+
 
